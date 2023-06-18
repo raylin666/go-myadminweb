b14ad302829250ef97c2e7ac5c5d3ec2c32f2865
linshan <1099013371@qq.com>
2023-06-18 21:54:55 +0800

    fix: 无用代码删除

--- src/api/account.ts
+++ src/api/account.ts
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import type { RouteRecordNormalized } from 'vue-router';
 import { UserState } from '@/store/modules/user/types';
 
 export interface LoginRequest {
@@ -24,8 +23,4 @@ export function logout() {
 export function getUserInfo() {
   axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
   return axios.get<UserState>('/account/info');
-}
-
-export function getMenuList() {
-  return axios.post<RouteRecordNormalized[]>('/api/user/menu');
 }
